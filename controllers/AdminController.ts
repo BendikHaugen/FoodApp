7aa83648eb4f812ce00fdcad288aac28de9eed00
Bendik Haugen <bendik_haugen@hotmail.com>
2024-05-27 15:12:26 +0200

    added project start with some dummy vandor-functions

---
+++ controllers/AdminController.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from "express";
+import { CreateVandorInput } from "../dto";
+
+const CreateVandor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const {
+    name,
+    address,
+    pincode,
+    foodType,
+    email,
+    password,
+    ownerName,
+    phone,
+  } = <CreateVandorInput>req.body;
+  return res.json({
+    name,
+    address,
+    pincode,
+    foodType,
+    email,
+    password,
+    ownerName,
+    phone,
+  });
+
+  res.json({ message: "Create Vandor" });
+};
+
+const GetVandors = async (req: Request, res: Response, next: NextFunction) => {
+  res.json({ message: "Get Vandors" });
+};
+
+const GetVanodrById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.json({ message: "Update Vandor" });
+};
+
+export { CreateVandor, GetVandors, GetVanodrById };
